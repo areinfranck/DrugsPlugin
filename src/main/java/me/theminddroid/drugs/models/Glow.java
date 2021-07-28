6bcfb75d14ff534d6120a2c484cee23ddfab3bd0
TheMindDroid <theminddroid@gmail.com>
2021-07-27 22:26:31 -0700

    Changed Glow.java to allow the plugin to return "Drug" enchantment string instead of null.

--- src/main/java/me/theminddroid/drugs/models/Glow.java
+++ src/main/java/me/theminddroid/drugs/models/Glow.java
@@ -39,7 +39,7 @@ public class Glow extends Enchantment {
     @Override
     public String getName() {
         // TODO Auto-generated method stub
-        return null;
+        return "Drug";
     }
 
     @Override
