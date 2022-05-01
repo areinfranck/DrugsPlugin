3a171a9916fbe46f59ad8b4e93ff809aa53065d2
TheMindDroid <theminddroid@gmail.com>
2022-04-30 21:42:24 -0700

    Introduced 6 new drugs and added crafting recipe for Narcan.

--- src/main/java/me/theminddroid/drugs/models/Drug.java
+++ src/main/java/me/theminddroid/drugs/models/Drug.java
@@ -13,7 +13,7 @@ public enum Drug {
             Material.MILK_BUCKET,
             "narcan",
             new DrugType.Narcan(),
-            new DrugRecipe.None()),
+            new DrugRecipe.VerticalShaped(Material.ENDER_EYE, Material.DIAMOND, Material.BUCKET)),
     Cocaine(
             Material.SUGAR,
             "speed",
@@ -47,8 +47,51 @@ public enum Drug {
             "luck",
             new DrugType.PsychoActive(Sound.ENTITY_GENERIC_DRINK,
                     new DrugEffect(PotionEffectType.LUCK, "gained luck"),
-                    new DrugEffect(PotionEffectType.POISON, "are poisoned")),
-            new DrugRecipe.VerticalShaped(Material.WHEAT, Material.SWEET_BERRIES, Material.GLASS_BOTTLE));
+                    new DrugEffect(PotionEffectType.UNLUCK, "are unlucky")),
+            new DrugRecipe.VerticalShaped(Material.WHEAT, Material.SWEET_BERRIES, Material.GLASS_BOTTLE)),
+    Ketamine(
+            Material.LIGHT_GRAY_DYE,
+            "jump boost",
+            new DrugType.PsychoActive(Sound.ENTITY_PLAYER_BURP,
+                    new DrugEffect(PotionEffectType.JUMP, "gained a jump boost"),
+                    new DrugEffect(PotionEffectType.SLOW, "gained a speed reduction")),
+            new DrugRecipe.VerticalShaped(Material.QUARTZ, Material.GUNPOWDER, Material.PAPER)),
+    Marijuana(
+            Material.GREEN_DYE,
+            "resistance",
+            new DrugType.PsychoActive(Sound.ENTITY_PLAYER_BURP,
+                    new DrugEffect(PotionEffectType.DAMAGE_RESISTANCE, "gained damage resistance"),
+                    new DrugEffect(PotionEffectType.WEAKNESS, "have become weak")),
+            new DrugRecipe.VerticalShaped(Material.GREEN_DYE, Material.WHEAT, Material.PAPER)),
+    LSD(
+            Material.PAPER,
+            "night vision",
+            new DrugType.PsychoActive(Sound.ENTITY_PLAYER_BURP,
+                    new DrugEffect(PotionEffectType.NIGHT_VISION, "gained night vision"),
+                    new DrugEffect(PotionEffectType.BLINDNESS, "have become blind")),
+            new DrugRecipe.VerticalShaped(Material.SPIDER_EYE, Material.BLACK_DYE, Material.PAPER)),
+    Ecstasy(
+            Material.PHANTOM_MEMBRANE,
+            "levitation",
+            new DrugType.PsychoActive(Sound.ENTITY_PLAYER_BURP,
+                    new DrugEffect(PotionEffectType.LEVITATION, "gained levitation"),
+                    new DrugEffect(PotionEffectType.SLOW_FALLING, "gained slow falling")),
+            new DrugRecipe.VerticalShaped(Material.CHORUS_FRUIT, Material.PRISMARINE_CRYSTALS, Material.PAPER)),
+    Ayahuasca(
+            Material.BEETROOT_SOUP,
+            "invisibility",
+            new DrugType.PsychoActive(Sound.ENTITY_GENERIC_DRINK,
+                    new DrugEffect(PotionEffectType.INVISIBILITY, "gained invisibility"),
+                    new DrugEffect(PotionEffectType.GLOWING, "gained glowing")),
+            new DrugRecipe.VerticalShaped(Material.BEETROOT, Material.VINE, Material.BOWL)),
+    Shrooms(
+            Material.RED_MUSHROOM,
+            "health boost",
+                    new DrugType.PsychoActive(Sound.ENTITY_PLAYER_BURP,
+                    new DrugEffect(PotionEffectType.HEALTH_BOOST, "gained health boost"),
+                    new DrugEffect(PotionEffectType.POISON, "gained poison")),
+            new DrugRecipe.VerticalShaped(Material.RED_MUSHROOM, Material.BLAZE_POWDER, Material.PAPER)
+            );
 
     private static final Map<String, Drug> byDisplayName = new HashMap<>();
     private static final Map<String, Drug> byLowerCaseName = new HashMap<>();
